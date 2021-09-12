da424e6016707bc1451eb353d3c3cbff067b19bc
ChuQiliang <chu_qiliang@sina.com>
2021-09-12 10:34:31 +0800

    #chuqiliang 新增时间

--- src/chutest/java/Hello.java
+++ src/chutest/java/Hello.java
@@ -1,5 +1,8 @@
 package chutest.java;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author ChuQiliang
  * @date 2021/7/24-22:51
@@ -7,5 +10,8 @@ package chutest.java;
 public class Hello {
     public  static  void main(String[] args){
         System.out.println("Hello, ChuQiliang!");
+        SimpleDateFormat currentdate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println(currentdate.format(new Date()));
     }
 }
